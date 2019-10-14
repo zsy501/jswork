c850b25127565ff7db991ca3a4cf40278c395207
zsy501 <lance2680@icloud.com>
2019-10-15 07:42:28 +0800

    555

--- work5/work5.js
+++ work5/work5.js
@@ -1,33 +1,37 @@
+//九九乘法空表
 let str = '<table border="1">'
-for(let i=1;i<10;++i){
-    str += '<tr>'
-    for(var j=1;j<=10;++j){
-    str += '<td>&nbsp;</td>'
-    }
-  str +='</tr>'
+for(let a=1;a<10;++a){
+    str +='<tr>'
+    for(var b=1;b<=10;++b){
+        //拼接单元格
+        str += '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>'
+        }
+        str +='</tr>'
 }
-str += '</table>'
+str +='<table>'
+//设置div的html文档内容
+document.getElementById('table').innerHTML=str
 
-document.getElementById('table1').innerHTML = str
-//倒九九乘法空白表
+//倒九九乘法空表
 str = '<table border="1">'
 for(let i=9;i>0;--i){
-    str += '<tr>'
+    str +='<tr>'
     for(var j=i;j>0;--j){
-        str +='<td>&nbsp;</td>'
+        str += '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>'
     }
-    str += '</tr>'
+    str +='</tr>'
 }
 str += '</table>'
 document.getElementById('table2').innerHTML = str
+
 //倒九九乘法表
 str = '<table border="1">'
-for(let i=9;i>0;--i){
-    str += '<tr>'
-    for(var j=i;j>0;--j){
-        str +='<td>' + j + '=' + i + '=' + ( j * j ) + '</td>'
+for(let c=9;c>0;--c){
+    str +='<tr>'
+    for(var d=c;d>0;--d){
+		str += '<td>' + d + '*' +c + '=' +(d*c)+'<td>'
     }
-    str += '</tr>'
+    str +='</tr>'
 }
 str += '</table>'
 document.getElementById('table3').innerHTML = str
