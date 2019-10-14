fa584ccd018593928e58e1955bf12dc20f3f48cc
zsy501 <lance2680@icloud.com>
2019-10-14 13:48:21 +0800

    654

--- work4/work4.js
+++ work4/work4.js
@@ -1,31 +1,32 @@
 document.write("<p>金字塔</p>")
-let level = prompt('请设置金字塔的层数')
+let level = prompt('请设置金字塔层数')
 level = parseFloat(level)&&Number(level)
 if (isNaN(level)){
-    alert('金字塔的层数是数字')
+    alert('金字塔层数是数字')
 }
 for(let i=1;i<=level;++i){
     let blank = level - i;
     for(let k=0;k<blank;++k){
-        document.write('&nbsp;');
+        document.write('&nbsp;')
     }
 
     let star = i * 2 - 1
-    for (let j=0;j<star;++j){
+    for(let j=0;j<star;++j){
         document.write('*')
     }
 
-document.write("<br>")
+    document.write("<br>")
 }
 
 let str = '<table border="1">'
-for(let i =1;i<10;++i){
+for(let i=1;i<10;++i){
     str += '<tr>'
     for(var j=1;j<=i;++j){
-        str +='<td>' + j + '*' + '=' + (j * j) + '</td>'
-    }
 
-str += '</tr>'
+        str += '<td>' +j + '*' + i + '=' + ( j * i ) + '</td>'
+    }
+    str += '</tr>'
 }
 str += '</table>'
+
 document.getElementById('table').innerHTML = str
---
+++ work5/work5.js
@@ -0,0 +1,33 @@
+let str = '<table border="1">'
+for(let i=1;i<10;++i){
+    str += '<tr>'
+    for(var j=1;j<=10;++j){
+    str += '<td>&nbsp;</td>'
+    }
+  str +='</tr>'
+}
+str += '</table>'
+
+document.getElementById('table1').innerHTML = str
+//倒九九乘法空白表
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>&nbsp;</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table2').innerHTML = str
+//倒九九乘法表
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>' + j + '=' + i + '=' + ( j * j ) + '</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
