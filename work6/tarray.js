fd2602f4e583e8b17a1a8c07df349d2b8d1e17fb
zsy501 <lance2680@icloud.com>
2019-10-17 00:37:04 +0800

    1

--- work6/tarray.js
+++ work6/tarray.js
@@ -1,10 +1,12 @@
-var  arr = [1,2,3,4,2,5,6,2,7,2];
+var arr = [1,2,3,4,2,5,6,7,8,2,8,4];
 document.getElementById('array').innerHTML = arr.toString()
-var  index = 0,newarr = [];
+
+var index = 0, newArr = [];
+
 for(var i in arr){
-    if(arr[i] !=2){
-        newarr[index] = arr[i];
-        ++index;
-    }
+    if(arr[i] != 2){
+    newArr[index] = arr[i];
+    ++index;
+   }
 }
-document.getElementById('transArray').innerHTML = newarr.toString()
+document.getElementById('transArray').innerHTML = newArr.toString()
