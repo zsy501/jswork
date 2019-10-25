cb4a5f98c093f33088284158010aeb22ef347191
zsy501 <lance6868@icloud,com>
2019-10-25 18:46:57 +0800

    55

---
+++ work7slide/utils.js
@@ -0,0 +1,49 @@
+function bubbleSort(str) {
+    let arr = str.split(',')
+    let xjtLog = []
+    for (let i =0; i < arr.length - 1;i++) {   
+        for (let j = 0; j <arr.length - i - 1;j++) {
+            xjtLog.push([arr.concat(),[j,j + 1]])
+            xjtLog.push([arr.concat(),[j,j + 1]])
+            xjtLog.push([arr.concat(),[j,j + 1]])
+            xjtLog.push([arr.concat(),[j,j + 1]])
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j+1],arr[j]]
+              xjtLog.push([arr.concat(),[j,j + 1]])
+            }
+        }
+    }
+    xjtLog.push([arr.concat(),[-1,-1]])
+    return xjtLog
+}
+function showLog(logValues,showElement) {
+    let str = ''
+    let{
+        done,
+        value: [row,pos]
+    } = logValues.next()
+    for (const key in row) {
+        let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' : ''
+        str += '<span style="font-size:' +row[key] * 20 + 'px;' + color + '">' + row[key] + '</span>'
+    }
+    showElement.innerHTML = str
+    if (pos[0] != -1)
+    setTimeout("showLog(logValues,showDiv)",500)
+}
+function insertSort(str){
+   let arr = str.split(',')    
+    let xjtLog = []
+    for (let i =1; i < arr.length ;i++){
+        for (let j = i; j >0 ;j--) {
+            xjtLog.push([arr.concat(),[j-1,j ]])
+            xjtLog.push([arr.concat(),[j-1,j ]])
+            xjtLog.push([arr.concat(),[j-1,j ]])
+            if (arr[j-1] > arr[j]) {
+                [arr[j-1], arr[j]] = [arr[j],arr[j-1]]
+                xjtLog.push([arr.concat(),[j-1,j]])
+            }
+        }
+    }
+    xjtLog.push([arr.concat(),[-1,-1]])
+    return xjtLog 
+}
