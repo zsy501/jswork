e125e6f6d1ca1f90054ff3a8269b6967d8b2b8b9
zsy501 <lance2680@icloud.com>
2019-11-01 13:50:21 +0800

    66

--- work10/index.js
+++ work10/index.js
@@ -1,23 +1,32 @@
-function mokey(){
-    let total = document.getElementById("monkeytotal").Value
+function monkey() {
+    let total = document.getElementById("monkeytotal").value 
     let kick = document.getElementById("monkeykick").value
-    total = parseInt(total) && Number(total)
-    kick = parseInt(kick) && Number(kick)
+    total = parseInt(total) &&Number(total)
+    kick = parseInt(kick) &&Number(kick)
     if(isNaN(total)||isNaN(kick)){
         alert('请输入数字')
         return
     }
     let monkey=[]
-    for (let i=1;i<=total;i++ ){
-        mokey.push(i)
+    for (let i=1;i<=total;i++ ) {
+        monkey.push(i)
     }
     let i=0
     while (monkey.length>1){
         i++;
         head = monkey.shift()
         if(i%kick!=0){
-            mokey.push(head);
+            monkey.push(head);
         }
     }
     document.getElementById('monkeyking').innerText = monkey[0]
+  }
+function stat(){
+    let str = document.getElementById("str").value
+    let obj = {}
+    str.split('').reduce((pre, lance) => {
+        pre[lance] ? pre[lance]++ : pre[lance] = 1
+        return obj
+    }, [])
+     document.getElementById('result').innerText = JSON.stringify(obj) 
 }
